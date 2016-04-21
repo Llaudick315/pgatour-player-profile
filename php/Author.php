aba0ccd9822a72b2e009254af2ca6ccc39187d5d
Lucas Lauick <llaudick@cnm.edu>
2016-04-21 12:48:40 -0600

    added content to Author.php

--- php/Author.php
+++ php/Author.php
@@ -7,7 +7,7 @@ namespace Edu\Cnm\Llaudick\Pgatour;
  *
  * @author Lucas Laudick <llaudick@cnm.edu>
  **/
-class author implements \JsonSrializable {
+class author implements \JsonSerializable {
 		/**
 		 * id for this Author; this is the primary key
 		 * @var int $authorId
@@ -35,11 +35,46 @@ class author implements \JsonSrializable {
 		private $saltContent;
 
 		/**
+		 * constructor for this author
+		 *
+		 * @param int|null $newAuthorId of this Author or null if new author
+		 * @param string $newEmailContent new value of email content
+		 * @param string $newHashContent string containing hash content
+		 * @param string $newNameContent string containing name of author
+		 * @param string $newSaltContent string containing salt content
+		 * @throws \InvalidArgumentException if the data types are not valid
+		 * @throws \RangeException if data does not fit in databse
+		 * @throws \TypeError if data does
+		 * @throws \Exception if other exceptions occur
+		 */
+		public function __construct(int $newAuthorId = null, string $newEmailContent, string $newHashContent, string $newNameContent, string $newSaltContent) {
+			try {
+				$this->setAuthorId($newAuthorId);
+				$this->setEmailContent($newEmailContent);
+				$this->setHashContent($newHashContent);
+				$this->setNameContent($newNameContent);
+				$this->setSaltContent($newSaltContent);
+			} catch(\InvalidArgumentException $invalidArgument) {
+				// rethrow the exception to the caller
+				throw(new \InvalidArgumentException($invalidArgument->getMessage(), 0, $invalidArgument));
+			} catch(\RangeException $range) {
+				// rethrow the exception to the caller
+				throw(new \RangeException($range->getMessage(), 0, $range));
+			} catch(\TypeError $typeError) {
+				// rethrow the exception to the caller
+				throw(new \TypeError($typeError->getMessage(), 0, $typeError));
+			} catch(\Exception $exception) {
+				// rethrow the exception to the caller
+				throw(new \Exception($exception->getMessage(), 0, $exception));
+			}
+		}
+
+		/**
 		 * accessor method for author id
 		 *
 		 * @return int|null value of author id
 		 **/
-		public function getauthorId() {
+		public function getAuthorId() {
 			return($this->authorId);
 		}
 
@@ -85,7 +120,7 @@ class author implements \JsonSrializable {
 			// verify the email
 			$newEmailContent = trim($newEmailContent);
 			$newEmailContent = filter_var($newEmailContent, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-			if(empty($newEmailContent) === true) {
+			if(empty($newEmailContent) === false) {
 				throw(new \InvalidArgumentException("email content is empty or insecure"));
 			}
 			// verify the email content will fit in the database
@@ -107,11 +142,13 @@ class author implements \JsonSrializable {
 		 * mutator method for the hash content
 		 *
 		 * @param string $newHashContent new value of hash content
-		 * @throws \RangeException if $newHashContent is > 128
+		 * @throws \RangeException if $newHashContent is !== 128
 		 **/
 		public function setHashContent(string $newHashContent) {
+			// verufy the hash content has correct characters
+			$newHashContent=ctype_xdigit($newHashContent);
 			// verify hash content will fit in database
-			if(strlen($newHashContent) > 128) {
+			if(strlen($newHashContent) !== 128) {
 				throw(new \RangeException("Hash is too long"));
 			}
 			// store hash content
@@ -156,14 +193,28 @@ class author implements \JsonSrializable {
 		 * mutator method for salt content
 		 *
 		 * @param string $newSaltContent new value of salt contnent
-		 * @throws \RangeException if $newSaltContent is > 64
+		 * @throws \RangeException if $newSaltContent is !== 64
 		 **/
 		public function setSaltContent(string $newSaltContent) {
-			// verify hash content will fit in database
-			if(strlen($newSaltContent) > 64) {
-				throw(new \RangeException("Salt content is to large"));
+			// verify the salt content has the correct characters
+			$newSaltContent=ctype_xdigit($newSaltContent);
+			// verify salt content is the right length
+			if(strlen($newSaltContent) !== 64) {
+				throw(new \RangeException("Salt content is wrong lenght"));
 			}
 			// store the salt content
 			$this->saltContent = $newSaltContent;
 		}
-	}
+		/**
+		 * formats the state variabels for Json serialization
+		 *
+		 * @return array resulting state variables to serialize
+		 **/
+		public function jsonSerialize() {
+			// TODO: Implement jsonSerialize() method.
+			$fields = get_object_vars($this);
+			unset($fields["hashContent"]);
+			unset($fields["saltContent"]);
+			return($fields);
+		}
+}
